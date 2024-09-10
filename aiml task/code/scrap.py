3a5a1b5e84376d980e6801e07ba6e1305aef16ee
sowmithri sriram <95630559+sriramsowmithri9807@users.noreply.github.com>
2024-09-10 22:39:18 +0530

    Update scrap.py

--- aiml task/code/scrap.py
+++ aiml task/code/scrap.py
@@ -1,10 +1,8 @@
-# Define the search query
-query = '$AAPL'  # Example: Apple Inc.
+query = '$AAPL'  
 
-# Scrape tweets
 tweets = tweepy.Cursor(api.search, q=query, lang='en', tweet_mode='extended').items(1000)
 
-# Store tweets in a list
+
 tweet_data = []
 for tweet in tweets:
     tweet_data.append(tweet.full_text)
